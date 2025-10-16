a48435a62bfff2e63a9da4ae7bf0563be06b1647
kawkoi <haruubusinesss@gmail.com>
2025-10-17 00:25:24 +0530

    update

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -5,36 +5,43 @@ import axios from "axios";
 const fromPubkey = new PublicKey("DfgcjkinjBpg2unCzmQsZQ5ELeMMXuCVdqkY6XAXMfwE");
 const connection = new Connection("https://solana-devnet.g.alchemy.com/v2/V2O-WWPQn9hNVj25_ON6z");
 
-async function sendSol() {
-  const ix = SystemProgram.transfer({
-    fromPubkey,
-    toPubkey: new PublicKey("FJvMS3kvjKRLUvTqgoZgxk1aoNBuWy546rRuLX3BHmZQ"),
-    lamports: Math.floor(0.01 * LAMPORTS_PER_SOL)
-  });
-
-  const tx = new Transaction().add(ix);
-
-  const { blockhash } = await connection.getLatestBlockhash();
-  tx.recentBlockhash = blockhash;
-  tx.feePayer = fromPubkey;
-
-  const serializedTx = tx.serialize({
-    requireAllSignatures: false,
-    verifySignatures: false
-  });
-  console.log(serializedTx);
-
-  await axios.post("/api/v1/txn/sign", {
-    message: serializedTx,
-    retry: false
-  });
-}
-
 function App() {
+  async function sendSol() {
+    const ix = SystemProgram.transfer({
+      fromPubkey,
+      toPubkey: new PublicKey("FJvMS3kvjKRLUvTqgoZgxk1aoNBuWy546rRuLX3BHmZQ"),
+      lamports: Math.floor(0.01 * LAMPORTS_PER_SOL)
+    });
+
+    const tx = new Transaction().add(ix);
+
+    const { blockhash } = await connection.getLatestBlockhash();
+    tx.recentBlockhash = blockhash;
+    tx.feePayer = fromPubkey;
+
+    const serializedTx = tx.serialize({
+      requireAllSignatures: false,
+      verifySignatures: false
+    });
+    console.log(serializedTx);
+
+    await axios.post("http://localhost:3000/api/v1/txn/sign", {
+      message: serializedTx,
+      retry: false
+    });
+  }
+
+
   return (
     <div className="flex items-center justify-center min-h-screen">
+      <input className="border border-gray-500"
+        type="text"
+        placeholder="Amount"></input>
+      <input className=" border border-gray500"
+        type="text"
+        placeholder="Address"></input>
       <button
-        onClick={() => sendSol().catch(err => console.error(err))}
+        onClick={() => sendSol()}
         className="rounded-lg border border-black px-4 py-2 bg-white text-black"
       >
         Send 0.01 SOL
